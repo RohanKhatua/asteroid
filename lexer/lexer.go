a9b9c518dc280dcfa907373340428782613e31fe
RohanKhatua <contact@rohankhatua.dev>
2025-01-18 18:04:25 +0530

    feat: first tests pass - token generation passes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "asteroid/token"
+import (
+	"asteroid/token"
+	"asteroid/utils"
+)
 
 type Lexer struct {
 	input        string
@@ -45,6 +48,9 @@ func (l *Lexer) readChar() {
 func (l *Lexer) nextToken() token.Token {
 	var tok token.Token
 	// check the current character and assign the token type accordingly
+
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -66,6 +72,21 @@ func (l *Lexer) nextToken() token.Token {
 		// Default case - EOF
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if utils.IsLetter(l.ch) {
+			// if the character is a letter - read the identifier
+			tok.Literal = l.readIdentifier()
+			// check if the identifier is a keyword - if it is set the token type to the keyword type
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if utils.IsDigit(l.ch) {
+			// if the character is a digit - read the number
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -78,4 +99,37 @@ func (l *Lexer) nextToken() token.Token {
 // We are doing it here to keep the lexer and token packages separate and to maintain consistency with the book.
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// readIdentifier reads an identifier and advances the lexer's position until it encounters a non-letter character.
+// It then returns the identifier.
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for utils.IsLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+// readNumber reads a number and advances the lexer's position until it encounters a non-digit character.
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for utils.IsDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// It does not take a genius to figure out that the readIdentifier and readNumber functions are very similar.
+// They only differ in the condition that they check for - IsLetter and IsDigit respectively.
+// We can refactor this to a single function that takes a condition as an argument.
+// However, we will keep it as is for now as I want to keep the code readable for myself.
+
+// skipWhitespace skips over any whitespace characters.
+func (l *Lexer) skipWhitespace() {
+	// skip over any whitespace characters.
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
